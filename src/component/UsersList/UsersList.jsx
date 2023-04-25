47a87f6de8a019b2c1177bb4618921a5bb352f19
Volodymyr <mospan.volodymyr@gmail.com>
2023-04-25 21:04:05 +0300

    fixes some bags

--- src/component/UsersList/UsersList.jsx
+++ src/component/UsersList/UsersList.jsx
@@ -1,26 +1,13 @@
 import { UserCard } from 'component/UserCard/UserCard';
 import { UsersListItem, UsersListStyled } from './UsersList.styled';
-import { useEffect, useRef } from 'react';
 
 export const UsersList = ({ users, onClick, followingsUsers, isLoading }) => {
-  const lastUserRef = useRef(null);
-  let ref = lastUserRef.current;
-
-  useEffect(() => {
-    if (ref) {
-      ref.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [ref]);
-
   return (
     <UsersListStyled>
-      {users.map((user, index) => {
+      {users.map(user => {
         const isFollowing = followingsUsers.includes(user.id);
         return (
-          <UsersListItem
-            key={user.id}
-            ref={index === users.length - 1 ? lastUserRef : null}
-          >
+          <UsersListItem key={user.id}>
             <UserCard
               userId={user.id}
               tweets={user.tweets}
